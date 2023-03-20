6836e060818e2c8ba49334351468eacf706be7dc
JawaharCoder <jawaharcoder@gmail.com>
2023-03-20 21:18:36 +0530

    Products Added

--- shoppingcartreact/src/App.js
+++ shoppingcartreact/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
--- shoppingcartreact/src/pages/Home.jsx
+++ shoppingcartreact/src/pages/Home.jsx
@@ -1,86 +1,92 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import Button from 'react-bootstrap/Button';
+const PUBLIC_URL = process.env.PUBLIC_URL;
 
 function Home() {
     return (
         <div>
              <CardGroup>
                 <Card>
-                <Card.Img variant="top" style={{ paddingTop: '10px', width: '220px', height: '250px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src="https://cdn.shopify.com/s/files/1/0632/7880/9324/products/IMG-6590774_823x.jpg?v=1660904057" />
+                <Card.Img variant="top" style={{ paddingTop: '10px', width: '120px', height: '200px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src={ PUBLIC_URL + '/images/image1.png'} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>APPLE iPhone 13 (Midnight, 128 GB)</Card.Title>
                     <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This content is a little bit longer.
+                        iPhone 13. boasts an advanced dual-camera system that allows
+                        you to click mesmerising pictures with immaculate clarity. 
                     </Card.Text>
+                    <Button variant="link" style={{width: '100%'}}>Add to cart</Button>
                     </Card.Body>
                     <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
                     </Card.Footer>
                 </Card>
                 <Card>
-                <Card.Img variant="top" style={{ paddingTop: '10px', width: '220px', height: '250px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src="https://cdn.shopify.com/s/files/1/0632/7880/9324/products/IMG-6590774_823x.jpg?v=1660904057" />
+                <Card.Img variant="top" style={{ paddingTop: '10px', width: '120px', height: '200px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src={ PUBLIC_URL + '/images/image2.png'} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>APPLE iPhone 13 (Green, 256 GB)</Card.Title>
                     <Card.Text>
-                        This card has supporting text below as a natural lead-in to
-                        additional content.{' '}
+                        The iPhone 13 features a stylish design and innovative
+                        features that make it a delight to use.
                     </Card.Text>
+                    <Button variant="link" style={{width: '100%'}}>Add to cart</Button>
                     </Card.Body>
                     <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
                     </Card.Footer>
                 </Card>
                 <Card>
-                <Card.Img variant="top" style={{ paddingTop: '10px', width: '220px', height: '250px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src="https://cdn.shopify.com/s/files/1/0632/7880/9324/products/IMG-6590774_823x.jpg?v=1660904057" />
+                <Card.Img variant="top" style={{ paddingTop: '10px', width: '120px', height: '200px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src={ PUBLIC_URL + '/images/image3.png'} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>APPLE iPhone 13 ((PRODUCT)RED, 128 GB)</Card.Title>
                     <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This card has even longer content than the
-                        first to show that equal height action.
+                        iPhone 13. boasts an advanced dual-camera system 
+                        that allows you to click mesmerising pictures with immaculate clarity.
                     </Card.Text>
+                    <Button variant="link" style={{width: '100%'}}>Add to cart</Button>
                     </Card.Body>
                     <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
                     </Card.Footer>
                 </Card>
                 <Card>
-                <Card.Img variant="top" style={{ paddingTop: '10px', width: '220px', height: '250px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src="https://cdn.shopify.com/s/files/1/0632/7880/9324/products/IMG-6590774_823x.jpg?v=1660904057" />
+                <Card.Img variant="top" style={{ paddingTop: '10px', width: '120px', height: '200px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src={ PUBLIC_URL + '/images/image4.png'} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>APPLE iPhone 13 (Blue, 512 GB)</Card.Title>
                     <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This content is a little bit longer.
+                        iPhone 13. boasts an advanced dual-camera system that 
+                        allows you to click mesmerising pictures with immaculate clarity.
                     </Card.Text>
+                    <Button variant="link" style={{width: '100%'}}>Add to cart</Button>
                     </Card.Body>
                     <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
                     </Card.Footer>
                 </Card>
                 <Card>
-                <Card.Img variant="top" style={{ paddingTop: '10px', width: '220px', height: '250px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src="https://cdn.shopify.com/s/files/1/0632/7880/9324/products/IMG-6590774_823x.jpg?v=1660904057" />
+                <Card.Img variant="top" style={{ paddingTop: '10px', width: '120px', height: '200px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src={ PUBLIC_URL + '/images/image5.png'} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>APPLE iPhone 13 (Midnight, 512 GB)</Card.Title>
                     <Card.Text>
-                        This card has supporting text below as a natural lead-in to
-                        additional content.{' '}
+                        iPhone 13. boasts an advanced dual-camera 
+                        system that allows you to click mesmerising pictures with immaculate clarity.
                     </Card.Text>
+                    <Button variant="link" style={{width: '100%'}}>Add to cart</Button>
                     </Card.Body>
                     <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
                     </Card.Footer>
                 </Card>
                 <Card>
-                <Card.Img variant="top" style={{ paddingTop: '10px', width: '220px', height: '250px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src="https://cdn.shopify.com/s/files/1/0632/7880/9324/products/IMG-6590774_823x.jpg?v=1660904057" />
+                <Card.Img variant="top" style={{ paddingTop: '10px', width: '120px', height: '200px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src={ PUBLIC_URL + '/images/image6.png'} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>APPLE iPhone 13 (Midnight, 256 GB)</Card.Title>
                     <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This card has even longer content than the
-                        first to show that equal height action.
+                        iPhone 13. boasts an advanced dual-camera 
+                        system that allows you to click mesmerising pictures with immaculate clarity.
                     </Card.Text>
+                    <Button variant="link" style={{width: '100%'}}>Add to cart</Button>
                     </Card.Body>
                     <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
@@ -89,86 +95,93 @@ function Home() {
                 </CardGroup>
                 <CardGroup>
                 <Card>
-                <Card.Img variant="top" style={{ paddingTop: '10px', width: '220px', height: '250px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src="https://cdn.shopify.com/s/files/1/0632/7880/9324/products/IMG-6590774_823x.jpg?v=1660904057" />
+                <Card.Img variant="top" style={{ paddingTop: '10px', width: '120px', height: '200px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src={ PUBLIC_URL + '/images/image7.png'} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>APPLE iPhone 13 (Pink, 256 GB)</Card.Title>
                     <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This content is a little bit longer.
+                        iPhone 13. boasts an advanced dual-camera system 
+                        that allows you to click mesmerising pictures with immaculate clarity. 
                     </Card.Text>
+                    <Button variant="link" style={{width: '100%'}}>Add to cart</Button>
                     </Card.Body>
                     <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
                     </Card.Footer>
                 </Card>
                 <Card>
-                <Card.Img variant="top" style={{ paddingTop: '10px', width: '220px', height: '250px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src="https://cdn.shopify.com/s/files/1/0632/7880/9324/products/IMG-6590774_823x.jpg?v=1660904057" />
+                <Card.Img variant="top" style={{ paddingTop: '10px', width: '120px', height: '200px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src={ PUBLIC_URL + '/images/image8.png'} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>APPLE iPhone 13 (Starlight, 256 GB)</Card.Title>
                     <Card.Text>
-                        This card has supporting text below as a natural lead-in to
-                        additional content.{' '}
+                            iPhone 13. boasts an advanced dual-camera system that
+                             allows you to click mesmerising pictures with immaculate clarity.
                     </Card.Text>
+                    <Button variant="link" style={{width: '100%'}}>Add to cart</Button>
                     </Card.Body>
                     <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
                     </Card.Footer>
                 </Card>
                 <Card>
-                <Card.Img variant="top" style={{ paddingTop: '10px', width: '220px', height: '250px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src="https://cdn.shopify.com/s/files/1/0632/7880/9324/products/IMG-6590774_823x.jpg?v=1660904057" />
+                <Card.Img variant="top" style={{ paddingTop: '10px', width: '120px', height: '200px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src={ PUBLIC_URL + '/images/image9.png'} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>APPLE iPhone 13 (Blue, 128 GB)</Card.Title>
                     <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This card has even longer content than the
-                        first to show that equal height action.
+                        iPhone 13. boasts an advanced dual-camera system that 
+                        allows you to click mesmerising pictures with immaculate clarity.
                     </Card.Text>
+                    <Button variant="link" style={{width: '100%'}}>Add to cart</Button>
                     </Card.Body>
                     <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
                     </Card.Footer>
                 </Card>
                 <Card>
-                <Card.Img variant="top" style={{ paddingTop: '10px', width: '220px', height: '250px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src="https://cdn.shopify.com/s/files/1/0632/7880/9324/products/IMG-6590774_823x.jpg?v=1660904057" />
+                <Card.Img variant="top" style={{ paddingTop: '10px', width: '120px', height: '200px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src={ PUBLIC_URL + '/images/image10.png'} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>APPLE iPhone 13 (Starlight, 128 GB)</Card.Title>
                     <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This content is a little bit longer.
+                        iPhone 13. boasts an advanced dual-camera system that allows 
+                        you to click mesmerising pictures with immaculate clarity.
                     </Card.Text>
+                    <Button variant="link" style={{width: '100%'}}>Add to cart</Button>
                     </Card.Body>
                     <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
                     </Card.Footer>
                 </Card>
                 <Card>
-                <Card.Img variant="top" style={{ paddingTop: '10px', width: '220px', height: '250px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src="https://cdn.shopify.com/s/files/1/0632/7880/9324/products/IMG-6590774_823x.jpg?v=1660904057" />
+                <Card.Img variant="top" style={{ paddingTop: '10px', width: '120px', height: '200px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src={ PUBLIC_URL + '/images/image11.png'} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>APPLE iPhone 13 (Green, 128 GB)</Card.Title>
                     <Card.Text>
-                        This card has supporting text below as a natural lead-in to
-                        additional content.{' '}
+                        The iPhone 13 features a stylish design and innovative
+                         features that make it a delight to use.
                     </Card.Text>
+                    <Button variant="link" style={{width: '100%'}}>Add to cart</Button>
                     </Card.Body>
                     <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
                     </Card.Footer>
                 </Card>
                 <Card>
-                <Card.Img variant="top" style={{ paddingTop: '10px', width: '220px', height: '250px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src="https://cdn.shopify.com/s/files/1/0632/7880/9324/products/IMG-6590774_823x.jpg?v=1660904057" />
+                <Card.Img variant="top" style={{ paddingTop: '10px', width: '120px', height: '200px', margin: 'auto', alignItems: 'center', display: 'flex', justifyContent: 'center' }} src={ PUBLIC_URL + '/images/image12.png'} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>APPLE iPhone 13 (Pink, 128 GB)</Card.Title>
                     <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This card has even longer content than the
-                        first to show that equal height action.
+                        iPhone 13. boasts an advanced dual-camera system that 
+                        allows you to click mesmerising pictures with immaculate clarity.
                     </Card.Text>
+                    <Button variant="link" style={{width: '100%'}}>Add to cart</Button>
                     </Card.Body>
                     <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
                     </Card.Footer>
                 </Card>
                 </CardGroup>
+                <br/>
+                <br/>
+                <br/>
         </div>
     );
 }
